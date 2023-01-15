434f319346c62a3761e3777e96664e7afd5ff62d
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-15 08:19:46 +0800

    First commit

---
+++ Binary Search Tree Using Letters.py
@@ -0,0 +1,4 @@
+# Mark john M. Raymundo CoE 2-2 Data Structure and Algorithms
+# Instructions: Create a demo using the letters in your fullname as the content of the binary tree. 
+# Source: Part 1 https://www.youtube.com/watch?v=lFq5mYUWEBk Part 2 https://www.youtube.com/watch?v=JnrbMQyGLiU
+
