7634cab8a8954713befd3340f8e24e6d0b26e343
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-15 08:26:47 +0800

    Added add_child Definition

--- Binary Search Tree Using Letters.py
+++ Binary Search Tree Using Letters.py
@@ -3,4 +3,22 @@
 # Source: Part 1 https://www.youtube.com/watch?v=lFq5mYUWEBk Part 2 https://www.youtube.com/watch?v=JnrbMQyGLiU
 
 class BinarySearchTreeNode:
-    
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def add_child(self, data):
+    if data == self.data:
+        return
+
+    if data < self.data:
+        if self.left:
+            self.left.add_child(data)
+        else:
+            self.left = BinarySearchTreeNode(data)
+    else:
+        if self.right:
+            self.right.add_child(data)
+        else:
+            self.right = BinarySearchTreeNode(data)
